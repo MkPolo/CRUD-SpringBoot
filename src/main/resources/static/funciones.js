4f77232fa871cb659fa5674b7cb814a892d54d8d
MkPolo <mk.polo1708@gmail.com>
2020-04-15 19:05:01 -0500

    Se implemento el mensaje de confirmacion para eliminar

--- src/main/resources/static/funciones.js
+++ src/main/resources/static/funciones.js
@@ -1,3 +1,29 @@
 function eliminar(id) {
-	
+	Swal.fire({
+		  title: '¿Estás seguro?',
+		  text: "¡No podrás revertir esto!",
+		  icon: 'warning',
+		  showCancelButton: true,
+		  confirmButtonColor: '#3085d6',
+		  cancelButtonColor: '#d33',
+		  confirmButtonText: 'Si, eliminar!'
+		}).then((result) => {
+		  if (result.value) {
+			  $.ajax({
+				 url:"/eliminar/"+id,
+				 success: function(res) {
+					 
+				 }
+			  });
+		    Swal.fire(
+		      '¡Eliminado!',
+		      'Su registro ha sido eliminado.',
+		      'success'
+		    ).then((result)=>{
+		    	if(result.value){
+		    		location.href="/listar";
+		    	}
+		    })
+		  }
+		})
 }
