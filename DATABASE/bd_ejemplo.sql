5d5be5eb8535666feb151d004db19b38b7eee361
MkPolo <mk.polo1708@gmail.com>
2020-04-16 13:20:37 -0500

    Se adjunta el README.MD y la base de datos

---
+++ DATABASE/bd_ejemplo.sql
@@ -0,0 +1,49 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1:3306
+-- Tiempo de generación: 16-04-2020 a las 17:38:37
+-- Versión del servidor: 5.7.24
+-- Versión de PHP: 7.2.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `bd_ejemplo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `persona`
+--
+
+DROP TABLE IF EXISTS `persona`;
+CREATE TABLE IF NOT EXISTS `persona` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `telefono` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `persona`
+--
+
+INSERT INTO `persona` (`id`, `name`, `telefono`) VALUES
+(1, 'Mia', '987654');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ README.md
@@ -0,0 +1,14 @@
+# CRUD-SPRINGBOOT
+## MODULOS
+[[Introduccion](#introduccion)]
+[[Base de Datos](#database)]
+[[Interfaz](#Interfaz)]
+# Introduccion
+* Esto es un CRUD usando el framework Spring Boot.
+* Utilizamos las dependencias de: **Spring Boot JPA**, **Spring Web**, **MySQL Driver** y **Thymeleaf**.
+# Database
+* Estaremos utilizando **MySQL** como gestor de base de datos.
+*[Esta es la base de datos simple](DATABASE/bd_ejemplo.sql)
+# Interfaz
+* Para las interfaces estaremos utilizando: **Thymeleaf** y [**Bootstrap**](https://getbootstrap.com/).
+* Para mostrar **alertas personalizabales**, estaremos utilizando: [**SweetAlert**](https://sweetalert2.github.io/).
